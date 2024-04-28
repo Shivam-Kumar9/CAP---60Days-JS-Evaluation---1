57ccc2910e5db8f0456921f5fb7391432f6680b1
Your-Shivam <shivamkumar1019988@gmail.com>
2024-04-28 17:26:12 +0530

    all-filtering-feature

--- script.js
+++ script.js
@@ -6,19 +6,56 @@ let filSal = document.querySelector("#fil-salary")
 let tbody = document.querySelector("tbody")
 
 
+   // filter Department function
+let handleDepartment = ()=>{
+    let dep = filDep.value;
+    console.log(dep);
+    getData(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=1&limit=10&filterBy=department&filterValue=${dep}`)
+}
+
+filDep.addEventListener("change",()=>{
+    handleDepartment()
+}) 
+
+
+
+      // filter gender function   
+let handleGender = ()=>{
+    let gen = filGen.value;
+    console.log(gen);
+    getData(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=1&limit=10&filterBy=gender&filterValue=${gen}`)
+}
+
+filGen.addEventListener("change",()=>{
+    handleGender()
+}) 
+
+
+       // filter salary function
+let handleSalary = ()=>{
+    let sal = filSal.value;
+    console.log(sal);
+    getData(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=1&limit=10&filterBy=gender&filterValue=female&sort=salary&order=${sal}`)
+}
+
+filSal.addEventListener("change",()=>{
+    handleSalary()
+}) 
+ 
+
      // async fetching function
 let getData = async (url)=>{
 
     let res = await fetch(url) 
     let data = await res.json()
-    console.log(data)
+    //console.log(data)
     showData(data)
 }
      //function for display data on dom
 let showData = (arr)=>{
-
+      tbody.innerHTML=null;
     let orignaldata = arr.data;
-    console.log(orignaldata)
+    //console.log(orignaldata)
 
     orignaldata.forEach((ele,i)=>{
 
